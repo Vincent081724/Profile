e7de6c6de73638c9b3e3cd70b0d0da2f476a8b3e
Vincent081724 <vincentleyson24@gmail.com>
2026-03-07 22:15:32 +0800

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import MainPage from "./components/MainPage";
 function App() {
   return (
     <main>
-      <div className="min-h-screen w-full bg-slate-950 overflow-hidden px-40">
+      <div className="min-h-screen w-full bg-slate-950 overflow-hidden">
         <MainPage />
       </div>
     </main>
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,25 +1,76 @@
-import Logo from "../assets/Logo.png";
-
+import Header from "../components/Header";
+import Homepic from "../assets/HomePic.png";
+import AboutPic from "../assets/AboutPic.png";
 function MainPage() {
   return (
-    <div className="flex justify-between h-auto w-full bg-red-200 items-center py-6">
-      {/* Left side */}
-      <div className="flex flex-col sm:flex-row items-center text-lg text-white font-sans gap-5 ">
-        {/* Bigger logo */}
-        <img src={Logo} alt="Logo" className="h-[156px] w-[85px] mr-10" />
-        <p>Home</p>
-        <p>About</p>
-        <p>Services</p>
-        <p>Contact</p>
-        <p>Blog</p>
-      </div>
+    <div>
+      <Header />
+      <div className="grid grid-cols-2 w-full h-auto text-white px-40 items-center">
+        <div className="flex flex-col p-4">
+          <h2 className="text-4xl font-sans font-bold mb-2">
+            Design that Sells,
+          </h2>
 
-      {/* Right side */}
-      <div className="flex flex-col sm:flex-row items-center justify-center text-lg text-white font-sans gap-5 ">
-        <p>Login</p>
-        <p>Sign Up</p>
-        <p>Profile</p>
+          <h1 className="text-5xl font-sans font-bold mb-2">
+            Transform <span>Experiences</span>
+          </h1>
+
+          <p className="text-base font-sans font-regular mb-2">
+            Let your web design comes into life, do care most about satisfying
+            your needs, and will go the extra mile to meet your specific
+            requests. Half-cooked outputs aren’t an option for me. Thus, Get in
+            touch and see what i can work on you for today!
+          </p>
+
+          <button className="w-[130px] p-2 border rounded-xl m-2">
+            CONTACT US
+          </button>
+        </div>
+
+        <div className="p-4 flex justify-center items-center ">
+          <img src={Homepic} alt="Home Picture " className="h-[758px]" />
+        </div>
       </div>
+      <section id="about" className="w-full h-[1050px] text-white">
+        <div className="mx-40">
+          <div className="grid grid-cols-2 py-30">
+            <div className="p-4 ">
+              <img
+                src={AboutPic}
+                alt=""
+                className="border-30 rounded-xl border-gray-900"
+              />
+            </div>
+            <div className="p-4 ">
+              <h2 className="text-base text-gray-200 mt-2 pb-6">
+                Mastering Design Experience
+              </h2>
+              <h1 className="text-4xl ">
+                About <span>Me</span>
+              </h1>
+              <p className="my-8">
+                With over 10 years of immersive experience in the world of
+                design, I am your guide through the captivating realm of UI/UX
+                and Graphic & Web Design.
+              </p>
+              <p className="my-8">
+                My journey began with a passion for creating visually stunning
+                and intuitive interfaces, and it’s evolved into a relentless
+                pursuit of perfection in every pixel. From crafting
+                user-centered experiences to forging memorable brand identities,
+                I bring both expertise and artistry to the table.
+              </p>
+              <p className="my-8">
+                In this ever-evolving digital landscape, I’ve honed my skills to
+                adapt and innovate, ensuring your projects are always at the
+                forefront of design trends. My mission is clear: to elevate your
+                brand, engage your audience, and transform your vision into a
+                compelling reality.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
