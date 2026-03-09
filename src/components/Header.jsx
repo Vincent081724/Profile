41ee93338b10ad25ea8adf6a36374bbc7e4aec6e
Vincent081724 <vincentleyson24@gmail.com>
2026-03-09 23:19:06 +0800

    4th Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,60 @@
 import Logo from "../assets/Logo.png";
 
-function Header() {
+function Header({ setActiveSection }) {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
-    <div className="flex justify-between h-auto w-full bg-transparent items-center px-40 py-6 sticky top-0 z-50 bg-black/80 backdrop-blur-md">
+    <div className="flex justify-between w-full items-center px-80 py-6 sticky top-0 z-50 bg-transparent/80 backdrop-blur-md">
       {/* Left side */}
-      <div className="flex flex-col sm:flex-row items-center text-lg text-white font-sans gap-5 sticky top-0">
-        {/* Bigger logo */}
-        <img src={Logo} alt="Logo" className="h-[156px] w-[85px] mr-30" />
-        <p>Home</p>
-        <p>About</p>
-        <p>Services</p>
-        <p>Certification</p>
-        <p>Portfolio</p>
+      <img
+        src={Logo}
+        alt="Logo"
+        className="h-[136px] w-[95px] pr-6 cursor-pointer"
+        onClick={() => scrollToSection("home")}
+      />
+      <div className="flex items-center text-lg text-white font-sans gap-5">
+        <button
+          onClick={() => scrollToSection("home")}
+          className="cursor-pointer text-white hover:text-red-500 hover:border-b-2 hover:border-red-500 transition-all duration-200"
+        >
+          Home
+        </button>
+        <button
+          onClick={() => scrollToSection("about")}
+          className="cursor-pointer text-white hover:text-red-500 hover:border-b-2 hover:border-red-500 transition-all duration-200"
+        >
+          About
+        </button>
+        <button
+          onClick={() => scrollToSection("services")}
+          className="cursor-pointer text-white hover:text-red-500 hover:border-b-2 hover:border-red-500 transition-all duration-200"
+        >
+          Services
+        </button>
+        <button
+          onClick={() => scrollToSection("certification")}
+          className="cursor-pointer text-white hover:text-red-500 hover:border-b-2 hover:border-red-500 transition-all duration-200"
+        >
+          Certification
+        </button>
+        <button
+          onClick={() => scrollToSection("portfolio")}
+          className="cursor-pointer text-white hover:text-red-500 hover:border-b-2 hover:border-red-500 transition-all duration-200"
+        >
+          Portfolio
+        </button>
+        <div className="flex items-center text-lg text-white font-sans gap-5 pl-20">
+          <p>Login</p>
+          <p>Sign Up</p>
+          <p>Profile</p>
+        </div>
       </div>
 
       {/* Right side */}
-      <div className="flex flex-col sm:flex-row items-center justify-center text-lg text-white font-sans gap-5 ">
-        <p>Login</p>
-        <p>Sign Up</p>
-        <p>Profile</p>
-      </div>
     </div>
   );
 }
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -5,68 +5,83 @@ function MainPage() {
   return (
     <div>
       <Header />
-      <div className="grid grid-cols-2 w-full h-auto text-white px-40 items-center">
-        <div className="flex flex-col p-4">
-          <h2 className="text-4xl font-sans font-bold mb-2">
-            Design that Sells,
-          </h2>
+      <div>
+        <section
+          id="home"
+          className="min-h-screen grid grid-cols-2 w-full text-white px-80 items-center"
+        >
+          {/* Left content */}
+          <div className="flex flex-col p-4">
+            <h2 className="text-4xl font-sans font-bold mb-2">
+              Design that Sells,
+            </h2>
 
-          <h1 className="text-5xl font-sans font-bold mb-2">
-            Transform <span>Experiences</span>
-          </h1>
+            <h1 className="text-5xl font-sans font-bold mb-4 text-red-400">
+              Transform <span>Experiences</span>
+            </h1>
 
-          <p className="text-base font-sans font-regular mb-2">
-            Let your web design comes into life, do care most about satisfying
-            your needs, and will go the extra mile to meet your specific
-            requests. Half-cooked outputs aren’t an option for me. Thus, Get in
-            touch and see what i can work on you for today!
-          </p>
+            <p className="text-base font-sans my-8">
+              Let your web design comes into life, do care most about satisfying
+              your needs, and will go the extra mile to meet your specific
+              requests. Half-cooked outputs aren’t an option for me. Thus, Get
+              in touch and see what I can work on you for today!
+            </p>
 
-          <button className="w-[130px] p-2 border rounded-xl m-2">
-            CONTACT US
-          </button>
-        </div>
+            <button className="w-[130px] p-2 border rounded-xl mt-8">
+              CONTACT US
+            </button>
+          </div>
 
-        <div className="p-4 flex justify-center items-center ">
-          <img src={Homepic} alt="Home Picture " className="h-[758px]" />
-        </div>
+          {/* Right image */}
+          <div className="p-4 flex justify-center items-center">
+            <img
+              src={Homepic}
+              alt="Home Picture"
+              className="max-h-[700px] w-auto"
+            />
+          </div>
+        </section>
       </div>
-      <section id="about" className="w-full h-[1050px] text-white">
-        <div className="mx-40">
-          <div className="grid grid-cols-2 py-30">
-            <div className="p-4 ">
+      <section
+        id="about"
+        className="min-h-screen w-full text-white flex items-center px-80"
+      >
+        <div className="w-full">
+          <div className="grid grid-cols-2 gap-10">
+            <div className="flex justify-start items-center p-4">
               <img
                 src={AboutPic}
-                alt=""
-                className="border-30 rounded-xl border-gray-900"
+                alt="About Picture"
+                className="rounded-xl border-[30px] border-gray-900"
               />
             </div>
-            <div className="p-4 ">
-              <h2 className="text-lg text-gray-500 font-semibold mt-2 pb-6">
+
+            <div className="p-4">
+              <h2 className="text-lg text-gray-500 font-semibold pb-6">
                 Mastering Design Experience
               </h2>
+
               <h1 className="text-4xl font-bold">
                 About <span className="text-red-400">Me</span>
               </h1>
+
               <div className="font-sans text-base">
-                <p className="my-8">
+                <p className="my-6">
                   With over 10 years of immersive experience in the world of
                   design, I am your guide through the captivating realm of UI/UX
                   and Graphic & Web Design.
                 </p>
-                <p className="my-8">
+
+                <p className="my-6">
                   My journey began with a passion for creating visually stunning
                   and intuitive interfaces, and it’s evolved into a relentless
-                  pursuit of perfection in every pixel. From crafting
-                  user-centered experiences to forging memorable brand
-                  identities, I bring both expertise and artistry to the table.
+                  pursuit of perfection in every pixel.
                 </p>
-                <p className="my-8">
+
+                <p className="my-6">
                   In this ever-evolving digital landscape, I’ve honed my skills
                   to adapt and innovate, ensuring your projects are always at
-                  the forefront of design trends. My mission is clear: to
-                  elevate your brand, engage your audience, and transform your
-                  vision into a compelling reality.
+                  the forefront of design trends.
                 </p>
               </div>
             </div>
